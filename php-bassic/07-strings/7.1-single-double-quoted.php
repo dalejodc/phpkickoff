70c4b67c699eabf3ba0077e2fc37c1fedda2eb9e
Daniel Díaz <dalejodc@gmail.com>
2019-04-19 11:49:43 -0600

    Differences  vrs ''

---
+++ php-bassic/07-strings/7.1-single-double-quoted.php
@@ -0,0 +1,12 @@
+<?php
+
+$my_str = "World";
+
+echo "Hello $my_str! <br>";     //Displays: Hello World!
+echo 'Hello $my_str! <br>';     //Displays: Hello $my_str!
+
+echo 'Hello \t World! <br>';    // Displays: Hello\tWorld!
+echo "Hello\t$my_str! <br>";    // Displays: Hello   World!
+echo "I\'ll be back";           // Displays: I'll be back
+
+?>
