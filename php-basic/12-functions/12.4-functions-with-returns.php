e2fb1e6dcba56b471c0afa99329b195246c7c4ba
Daniel Díaz <dalejodc@gmail.com>
2019-04-20 12:17:27 -0600

    Function with returns

---
+++ php-basic/12-functions/12.4-functions-with-returns.php
@@ -0,0 +1,12 @@
+<?php
+
+// Defining function
+function getSum($num1, $num2){
+    $total = $num1 + $num2;
+    return $total;
+}
+ 
+// Printing returned value
+echo getSum(5, 10); // Outputs: 15
+
+?>
