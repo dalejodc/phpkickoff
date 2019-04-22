97f6e4e27345304f4a1ae25d02b87cde8d3ef173
Daniel Díaz <dalejodc@gmail.com>
2019-04-21 20:33:46 -0600

    date()

---
+++ php-intermediate/13-formatting-dates/13.1-date().php
@@ -0,0 +1,9 @@
+<?php
+echo date("d/m/Y") . "<br>";
+echo date("d-m-Y") . "<br>";
+echo date("d.m.Y") . "<br>";
+
+echo date("h:i:s") . "<br>";
+echo date("F d, Y h:i:s A") . "<br>";
+echo date("h:i a");
+?>
